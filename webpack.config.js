c89541b1fe73250f030b9643082c110c77012e27
Artyom Yakimov <ykim-49464@mail.ru>
2020-03-25 15:46:20 +0300

    2commit dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,42 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './js/main.js',
+  entry: {
+    bundle: [
+      './src/main.js', './src/scss/main.scss'
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
-
   module: {
       rules: [
           {
               test: /\.js$/,
               use: ['babel-loader']
-          }
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              MiniCssExtractPlugin.loader, 
+              'css-loader',
+              {
+                loader: 'css-loader',
+                options: { sourceMap: true }
+              },
+              {
+                loader: 'sass-loader',
+                options: { sourceMap: true }
+              },
+            ]
+          },
       ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+  ],
 };
